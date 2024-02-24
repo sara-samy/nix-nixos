59d967aa4528236365379303f43e083d2a2a5500
sara-samy <intermsofart@hotmail.com>
2024-02-24 02:10:10 +0100

    hm: source zsh plugins

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -8,7 +8,7 @@
       styles = {
         comment = "fg=black,bold";
 	path = "fg=cyan";
-	};
+      };
     };
     history = {
       save = 1000;
@@ -17,9 +17,26 @@
       ignoreDups = true;
       ignoreSpace = true;
     };
-    initExtra = ''
-      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-      '';
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+	src = pkgs.fetchFromGitHub {
+	  owner = "zsh-users";
+	  repo = "zsh-autosuggestions";
+	  rev = "v0.7.0";
+	  hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+	};
+      }
+      {
+        name = "zsh-vi-mode";
+	src = pkgs.fetchFromGitHub {
+	  owner = "zsh-users";
+	  repo = "zsh-vi-mode";
+	  rev = "v0.11.0";
+	  hash = "sha256-xbchXJTFWeABTwq6h4KWLh+EvydDrDzcY9AQVK65RS8=";
+        };
+      }
+    ];
     shellAliases = import ./aliases.nix {inherit pkgs lib config;};
-    };
+  };
 }
