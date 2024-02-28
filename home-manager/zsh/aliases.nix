b203e5d2502f77bfce8b74c008b1976c022ab8bb
sara-samy <intermsofart@hotmail.com>
2024-02-29 00:41:11 +0100

    hm: update zsh aliases

--- home-manager/zsh/aliases.nix
+++ home-manager/zsh/aliases.nix
@@ -4,6 +4,7 @@
     cat = "${getExe bat} --style=plain";
     tree = "${getExe eza} --tree --icons --tree";
     rebuild-os = "sudo nixos-rebuild switch --flake $HOME/nixos-config/flake.nix#nixos";
+    rollback-once = "sudo nixos-rebuild --rollback switch --flake $HOME/nixos-config/flake.nix#nixos";
     yt-mp3 = ''
       ${getExe yt-dlp} -x --continue --add-metadata --embed-thumbnail --audio-format mp3 --audio-quality 0 --metadata-from-title="%(artist)s - %(title)s" --prefer-ffmpeg -o "%(title)s.%(ext)s"'';
 }
