9105eeae302390f2f844e70a64a73bf480f485a7
sara-samy <intermsofart@hotmail.com>
2024-03-21 18:18:51 +0100

    refactor git

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -1,16 +1,32 @@
-{config, pkgs, ... }: {
+{
+  lib,
+  username,
+  ...
+}: {
+  # `programs.git` will generate the config file: ~/.config/git/config
+  # to make git use this config file, `~/.gitconfig` should not exist!
+  home.activation.removeExistingGitconfig = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+    rm -f ~/.gitconfig
+  '';
+
   programs.git = {
     enable = true;
+    lfs.enable = true;
+
+    userName = "sara-samy";
+    userEmail = "intermsofart@hotmail.com";
+
     extraConfig = {
-      init = {defaultBranch = "main";};
+      init.defaultBranch = "main";
+      push.default = "current";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
     };
-    includes = [
-      { path = ./gitconfig; }
-      # { condition = "gitdir:sarasamygitlab/"; path = ./gitconfig-work; }
-    ];
+
     ignores = [
       ".cache/"
       ".DS_Store"
+      "Icon?"
       ".idea/"
       ".vscode"
       ".direnv/"
@@ -18,6 +34,21 @@
       ".ipynb_checkpoints"
       "__pycache__"
       "*.blend1"
+      ".gitconfig"
     ];
+
+    aliases = {
+      stash = "stash --all";
+      uncommit = "reset --soft HEAD~1";
+      checkout-remote = "checkout --track";
+
+      delete-remote-branch = "push origin --delete";
+      delete-local-branch = "branch -d";
+
+      unpushed = "!GIT_CURRENT_BRANCH=$(git name-rev --name-only HEAD) && git log origin/$GIT_CURRENT_BRANCH..$GIT_CURRENT_BRANCH --oneline";
+
+      log = "log --abbrev-commit";
+      logline = "log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --decorate=full";
+    };
   };
 }
